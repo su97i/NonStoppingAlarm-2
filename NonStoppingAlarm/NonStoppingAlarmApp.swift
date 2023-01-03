b13bf3f877d0e52eb367b1d19a314dd26a0ffa31
Shoug <Shoug.turki1@gmail.com>
2023-01-03 12:14:08 +0300

    Initial Commit

---
+++ NonStoppingAlarm/NonStoppingAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  NonStoppingAlarmApp.swift
+//  NonStoppingAlarm
+//
+//  Created by Shouq Turki Bin Tuwaym on 03/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct NonStoppingAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
